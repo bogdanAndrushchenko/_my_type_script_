5633df26e28478686cafc06fbf21daa570ad2a02
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2021-08-06 18:25:10 +0300

    refactor 04, 05

--- src/01-06_hw_js_to_ts/04hw.ts
+++ src/01-06_hw_js_to_ts/04hw.ts
@@ -39,44 +39,44 @@ const connect = "connect 04"
 
 //// task 2
 
-interface I_inventory {
-    items: string[]
-    add(itemName: string): void
-    remove(itemName: string): void
-}
-
-type callback = (itemName: string)=> void;
-type func = (itemName:string, action:callback)=> void;
-
-const inventory:I_inventory = {
-    items: ['Knife', 'Gas mask'],
-    add(itemName) {
-        console.log(`Adding ${itemName} to inventory`);
-
-        this.items.push(itemName);
-    },
-    remove(itemName) {
-        console.log(`Removing ${itemName} from inventory`);
-
-        this.items = this.items.filter(item => item !== itemName);
-    },
-};
-
-const invokeInventoryAction:func = function (itemName, action) {
-    console.log(`Invoking action on ${itemName}`);
-    action.call(inventory,itemName)
-};
-
-invokeInventoryAction('Medkit', inventory.add);
-// Invoking action on Medkit
-// Adding Medkit to inventory
-
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-
-invokeInventoryAction('Gas mask', inventory.remove);
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
-
-console.log(inventory.items); // ['Knife', 'Medkit']
+// interface I_inventory {
+//     items: string[]
+//     add(itemName: string): void
+//     remove(itemName: string): void
+// }
+//
+// type callback = (itemName: string)=> void;
+// type func = (itemName:string, action:callback)=> void;
+//
+// const inventory:I_inventory = {
+//     items: ['Knife', 'Gas mask'],
+//     add(itemName) {
+//         console.log(`Adding ${itemName} to inventory`);
+//
+//         this.items.push(itemName);
+//     },
+//     remove(itemName) {
+//         console.log(`Removing ${itemName} from inventory`);
+//
+//         this.items = this.items.filter(item => item !== itemName);
+//     },
+// };
+//
+// const invokeInventoryAction:func = function (itemName, action) {
+//     console.log(`Invoking action on ${itemName}`);
+//     action.call(inventory,itemName)
+// };
+//
+// invokeInventoryAction('Medkit', inventory.add);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+//
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+//
+// invokeInventoryAction('Gas mask', inventory.remove);
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+//
+// console.log(inventory.items); // ['Knife', 'Medkit']
 
 export {connect}
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 // import { connect} from "./01-06_hw_js_to_ts/01hw";
 // import {connect} from "./01-06_hw_js_to_ts/02hw";
 // import {connect} from "./01-06_hw_js_to_ts/03hw";
-import {connect} from "./01-06_hw_js_to_ts/04hw";
+// import {connect} from "./01-06_hw_js_to_ts/04hw";
+import {connect} from "./01-06_hw_js_to_ts/05hw";
 
 console.log(connect)
 // let a:string = '4';
