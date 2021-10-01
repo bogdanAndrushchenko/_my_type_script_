8d2330577ec9bc41c7cf59b3d41568a918a057c1
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2021-10-01 18:15:13 +0300

    try generic

--- src/01-06_hw_js_to_ts/06hw.ts
+++ src/01-06_hw_js_to_ts/06hw.ts
@@ -11,21 +11,21 @@ console.log(getUserNames(arr));
 const getUsersWithEyeColor: FilteredArr = (users, color) => {
     return users.filter(el => el.eyeColor === color)
 };
-const getUsersWithGender = (users: IArray[], gender: string): string[] => {
+const getUsersWithGender = (users:IArray[] , gender: string) => {
     return users.filter(el => el.gender === gender).map(e => e.name)
 };
 const getInactiveUsers = (users: IArray[]) => {
-    return<IArray[]> users.filter(el=>!el.isActive)
+    return users.filter(el => !el.isActive)
 };
-const getUserWithEmail = (users:IArray[], email:string) => {
-    return <IArray>users.find(el => el.email === email)
+const getUserWithEmail = (users: IArray[], email: string) => {
+    return users.find(el => el.email === email)
 };
-const getUsersWithAge = (users:IArray[], min:number, max:number):IArray[] => {
-    return users.filter(el=>el.age>=min && el.age<=max)
+const getUsersWithAge = (users: IArray[], min: number, max: number): IArray[] => {
+    return users.filter(el => el.age >= min && el.age <= max)
 };
 
 
-const s: string[] = getUsersWithGender(arr, 'male')
+const s = getUsersWithGender(arr, 'male')
 console.log(s, 's')
 console.log(getUsersWithGender(arr, 'male'));
 console.log(getUsersWithEyeColor(arr, 'blue'));
@@ -34,3 +34,6 @@ console.log(getUserWithEmail(arr, 'shereeanthony@kog.com'));
 console.log(getUserWithEmail(arr, 'elmahead@omatom.com'));
 console.log(getUsersWithAge(arr, 20, 30));
 console.log(getUsersWithAge(arr, 30, 40));
+const a = {a: 1, b: 2}
+const b = {a: 1, b: 2}
+console.log(Object.is(1, b))
